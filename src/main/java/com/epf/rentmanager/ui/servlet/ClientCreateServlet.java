f6e9369866615fac366f68a8e95796fe736d55a2
Naomi <naomi.kitch@epfedu.fr>
2024-03-11 15:23:59 +0100

    echec tentative ClientServlet

---
+++ src/main/java/com/epf/rentmanager/ui/servlet/ClientCreateServlet.java
@@ -0,0 +1,49 @@
+package com.epf.rentmanager.ui.servlet;
+
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.model.Client;
+import com.epf.rentmanager.model.Vehicle;
+import com.epf.rentmanager.service.ClientService;
+import com.epf.rentmanager.service.VehicleService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+
+@WebServlet("/users/create")
+public class ClientCreateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        this.getServletContext().getRequestDispatcher("/WEB-INF/views/users/create.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Client client = new Client(
+                0L,
+                req.getParameter("last_name"),
+                req.getParameter("first_name"),
+                req.getParameter("email"),
+
+
+        );
+        try {
+            ClientService.getInstance().create(client);
+        } catch (ServiceException e) {
+            throw new ServletException(e.getMessage());
+        }
+        resp.sendRedirect(req.getContextPath() + "/cars/create");
+    }
+}
+*/
