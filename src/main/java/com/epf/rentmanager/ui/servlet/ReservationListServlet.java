20df85bc4323171a8f5fee9b436a7746f7c869fd
Naomi <naomi.kitch@epfedu.fr>
2024-03-13 15:12:13 +0100

    servlet list users et rent

--- src/main/java/com/epf/rentmanager/ui/servlet/ReservationListServlet.java
+++ src/main/java/com/epf/rentmanager/ui/servlet/ReservationListServlet.java
@@ -1,4 +1,40 @@
 package com.epf.rentmanager.ui.servlet;
 
-public class ReservationListServlet {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.model.Client;
+import com.epf.rentmanager.model.Reservation;
+import com.epf.rentmanager.service.ClientService;
+import com.epf.rentmanager.service.ReservationService;
+import com.epf.rentmanager.service.VehicleService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/rents/list")
+public class ReservationListServlet extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        VehicleService vehicleService = VehicleService.getInstance();
+        List<Reservation> reservations = new ArrayList<Reservation>();
+
+        try {
+            reservations = ReservationService.getInstance().findAll();
+        } catch (ServiceException e) {
+            throw new ServletException(e.getMessage());
+        }
+        request.setAttribute("rents", reservations );
+        this.getServletContext().getRequestDispatcher("/WEB-INF/views/rents/list.jsp").forward(request, response);
+
+    }
+
 }
