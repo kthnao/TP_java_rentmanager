652eac4809c6fc6c75320dfac67702e3ad0cb4c0
Kitch Naomi <naomi@macbook-pro-de-kitch.home>
2024-04-06 13:31:41 +0200

    client update avec conditions check

--- src/main/java/com/epf/rentmanager/ui/servlet/Client/ClientUpdateServlet.java
+++ src/main/java/com/epf/rentmanager/ui/servlet/Client/ClientUpdateServlet.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
 
 @WebServlet("/users/update")
@@ -50,14 +51,31 @@ public class ClientUpdateServlet extends HttpServlet {
                 req.getParameter("first_name"),
                 req.getParameter("email"),
                 LocalDate.parse(req.getParameter("birthdate"), formatter));
+        boolean estMajeur = false;
+        boolean emailDispo = false;
+        boolean nomPrenomValide = false;
 
-
+        Client client2;
         try {
-            clientService.update(client);
+            client2 = clientService.findById(clientId).get();
+            estMajeur = clientService.estMajeur(client);
+            if (estMajeur) emailDispo = clientService.emailDispo(client);
+            if (emailDispo || Objects.equals(client.email(), client2.email())) nomPrenomValide = clientService.name_lenght(client);
+            if (nomPrenomValide) clientService.update(client);
         } catch (ServiceException e) {
-            throw new RuntimeException(e);
+            throw new ServletException(e);
         }
-        resp.sendRedirect(req.getContextPath() + "/users/list");
-
+        if (!estMajeur) {
+            req.setAttribute("clientError", "L'utilisateur doit être majeur.");
+            doGet(req, resp);
+        } else if (!emailDispo && !Objects.equals(client.email(), client2.email())) {
+            req.setAttribute("clientError", "L'email est déjà utilisé.");
+            doGet(req, resp);
+        } else if (!nomPrenomValide) {
+            req.setAttribute("clientError", "Le nom et le prénom doivent contenir au moins 3 caractères.");
+            doGet(req, resp);
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/users/list");
+        }
     }
 }
